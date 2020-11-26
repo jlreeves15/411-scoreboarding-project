56eb4546ea9978c50c07b1812069588dcdda000c
jlreeves15 <jreeves2@umbc.edu>
2020-11-25 19:29:21 -0500

    start of the project

---
+++ scoreboard.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+
+typedef struct 
+{
+
+}
+
+static struct 
+
+int main(argc, argv[])
+{
+
+}
