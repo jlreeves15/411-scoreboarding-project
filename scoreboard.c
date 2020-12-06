041910a767b3d2479d453432329f78aaa9fb4dc7
Cris Trinidad <ctrini1@umbc.edu>
2020-12-06 15:11:15 -0500

    modified:   scoreboard.c

--- scoreboard.c
+++ scoreboard.c
@@ -20,22 +20,24 @@ const int bufsize = BUFSIZ;
 static int vals_in_mem[] = {45,12,0,0,10,135,254,127,18,4,55,8,2,98,13,5,233,158, 167};
 static int int_reg[32] = {0};
 static float float_reg[32] = {0};
-//static int fp_adder = 0;
+static int fp_adder = 0;
 //static int fp_mul = 0;
 //static int fp_div = 0;
 static int int_calc = 0;
 
-void load_register(int dest, int offset, int address)
+void load_register(int dest, int offset, int address, int reg)
 {
     int counter = 0;
     if( offset + address <= 18)
     {
-	float_reg[dest] = vals_in_mem[offset + address];
+	if(reg == 1) float_reg[dest] = vals_in_mem[offset + address];
+	else int_reg[dest] = vals_in_mem[offset + address];
     }
     else if(offset + address > 18)
     {
 	counter = offset+address -18;
-	float_reg[dest] = vals_in_mem[counter - 1];
+	if(reg == 1) float_reg[dest] = vals_in_mem[counter - 1];
+	else int_reg[dest] = vals_in_mem[counter - 1];
     }
 }
 
@@ -111,6 +113,15 @@ int calculate( scoreboard_t instructions)
 {
     if(strcmp(instructions.inst, "L.D") == 0)
     {
+	int reg;
+	if(instructions.dest_reg[0] == 'F')
+	{
+	    reg = 1;
+	}
+	else
+	{
+	    reg = 2;
+	}
 	char dest[2];
 	int i = 1;
 	int j = 0;
@@ -128,11 +139,20 @@ int calculate( scoreboard_t instructions)
 	strcpy(offset, token);
 	token = strtok(NULL, ")");
 	strcpy(address, token);
-	load_register(atoi(dest), atoi(offset), atoi(address));
+	load_register(atoi(dest), atoi(offset), atoi(address), reg);
 	return 0;
     }
     else if(strcmp(instructions.inst, "S.D") == 0)
     {
+        int reg;
+        if(instructions.dest_reg[0] == 'F')
+        {
+            reg = 1;
+        }
+        else
+        {
+            reg = 2;
+        }
 	char dest[2];
         int i = 1;
         int j = 0;
@@ -150,7 +170,10 @@ int calculate( scoreboard_t instructions)
         strcpy(offset, token);
         token = strtok(NULL, ")");
         strcpy(address, token);
-        store_register(float_reg[atoi(dest)], atoi(offset), atoi(address));
+	if (reg == 1) store_register(float_reg[atoi(dest)],
+		atoi(offset), atoi(address));
+	else store_register(int_reg[atoi(dest)],
+                atoi(offset), atoi(address));
         return 0;
     }
     else if(strcmp(instructions.inst, "ADD") == 0)
@@ -414,7 +437,7 @@ int calculate( scoreboard_t instructions)
     }
 }
 
-int scoreboarding(scoreboard_t instructions[], char used_reg[bufsize][5],
+int scoreboarding(scoreboard_t instructions[], char used_reg[bufsize][3],
 	int counter, int score, int cur_inst)
 {
     int i;
@@ -427,7 +450,10 @@ int scoreboarding(scoreboard_t instructions[], char used_reg[bufsize][5],
 	{
 	    printf("Counter: %d\n",counter);
 	    int_calc = 1;
-            instructions[cur_inst].issue = score;
+
+	    if(instructions[cur_inst].issue == 0)
+            	instructions[cur_inst].issue = score;
+
 	    for( i = 0; i < bufsize; i++)
 	    {
 		if(strcmp(instructions[cur_inst].dest_reg, used_reg[i]) == 0)
@@ -477,10 +503,125 @@ int scoreboarding(scoreboard_t instructions[], char used_reg[bufsize][5],
 	    }
 	}
 	else if((strcmp(instructions[cur_inst].inst, "ADD") == 0 ||
-                	strcmp(instructions[cur_inst].inst, "SUB") == 0)
-                	&& int_calc == 0))
+                	strcmp(instructions[cur_inst].inst, "SUB") == 0 ||
+			strcmp(instructions[cur_inst].inst, "ADDI") == 0)
+                	&& int_calc == 0)
 	{
+	    printf("Counter: %d\n",counter);
+	    int_calc = 1;
+            instructions[cur_inst].issue = score;
 
+	    if(counter - 1 != -1)
+	    {
+		available = scoreboarding(instructions, used_reg, counter - 1,
+			score, cur_inst + 1);
+	    }
+	    instructions[cur_inst].read = ++score;
+	    instructions[cur_inst].execute = ++score;
+	    instructions[cur_inst].write = ++score;
+	    int_calc = 0;
+	    score++;
+
+	    if(available == 1 && counter - 1 != -1)
+	    {
+		printf("Yes\n");
+		scoreboarding(instructions, used_reg, counter - 1,
+			score, cur_inst + 1);
+	    }
+	}
+	else if(strcmp(instructions[cur_inst].inst, "ADD.D") == 0
+                        && int_calc == 0)
+	{
+	    printf("Counter: %d\n",counter);
+	    fp_adder = 1;
+
+	    if(instructions[cur_inst].issue == 0)
+            	instructions[cur_inst].issue = score;
+
+	    for( i = 0; i < bufsize; i++)
+	    {
+		if(strcmp(instructions[cur_inst].dest_reg, used_reg[i]) == 0)
+		{
+		    printf("No on cmp\n");
+		    return 1;
+		}
+		if(strcmp(instructions[cur_inst].source_reg1, used_reg[i]) == 0)
+                {
+                    printf("No on cmp\n");
+                    return 1;
+                }
+                if(strcmp(instructions[cur_inst].source_reg2, used_reg[i]) == 0)
+                {
+                    printf("No on cmp\n");
+                    return 1;
+                }
+		i++;
+	    }
+	    int count = 0;
+	    for( i = 0; i < bufsize; i++)
+            {
+                if(used_reg[i][0] == '\0')
+                {
+                    if( count == 0)
+			strcpy(used_reg[i], instructions[cur_inst].dest_reg);
+		    else if( count == 1)
+                        strcpy(used_reg[i], instructions[cur_inst].source_reg1);
+		    else if( count == 2)
+                        strcpy(used_reg[i], instructions[cur_inst].source_reg2);
+		    else
+			break;
+		    printf("On: %s\n",used_reg[i]);
+                }
+                i++;
+            }
+
+	    if(counter - 1 != -1)
+	    {
+		available = scoreboarding(instructions, used_reg, counter - 1,
+			score, cur_inst + 1);
+	    }
+
+	    instructions[cur_inst].read = ++score;
+	    score+=2;
+	    instructions[cur_inst].execute = score;
+	    instructions[cur_inst].write = ++score;
+	    fp_adder = 0;
+	    score++;
+
+	    for( i = 0; i < bufsize; i++)
+            {
+                if(strcmp(instructions[cur_inst].dest_reg, used_reg[i]) == 0)
+                {
+                    used_reg[i][0] = '\0';
+		    used_reg[i][1] = '\0';
+		    used_reg[i][2] = '\0';
+		    printf("No: %s\n",used_reg[i]);
+                }
+		else if(strcmp(instructions[cur_inst].source_reg1,
+				used_reg[i]) == 0)
+                {
+                    used_reg[i][0] = '\0';
+                    used_reg[i][1] = '\0';
+                    used_reg[i][2] = '\0';
+                    printf("No: %s\n",used_reg[i]);
+                }
+		else if(strcmp(instructions[cur_inst].source_reg2,
+				used_reg[i]) == 0)
+                {
+                    used_reg[i][0] = '\0';
+                    used_reg[i][1] = '\0';
+                    used_reg[i][2] = '\0';
+                    printf("No: %s\n",used_reg[i]);
+                }
+		i++;
+            }
+
+	    if(available == 1 && counter - 1 != -1)
+	    {
+	    	printf("Yes\n");
+		scoreboarding(instructions, used_reg, counter - 1,
+			score, cur_inst + 1);
+	    }
 	}
 	else { return 1; }
     }
@@ -560,7 +701,7 @@ int main()
 
     int score = 1;
     int cur_inst = 0;
-    char used_reg[bufsize][5];
+    char used_reg[bufsize][3];
     scoreboarding(my_scoreboard, used_reg, counter - 1, score, cur_inst);
 
         for(i = 0; i < counter - 1; i++)
@@ -572,21 +713,21 @@ int main()
         }
 
 
-	//int p;
-	//for(p = 0; p < counter - 1; p++)
-	//{
-	//	calculate(my_scoreboard[p]);
-	//}
+	int p;
+	for(p = 0; p < counter - 1; p++)
+	{
+		calculate(my_scoreboard[p]);
+	}
 	//printf("Float\n");
 	//for(p = 0; p < 32; p++)
 	//{
 	//	printf("%f\n", float_reg[p]);
         //}
-        //printf("Int\n");
-        //for(p = 0; p < 32; p++)
-        //{
-        //        printf("%d\n", int_reg[p]);
-        //}
+        printf("Int\n");
+        for(p = 0; p < 32; p++)
+        {
+                printf("%d\n", int_reg[p]);
+        }
 
 
 	return 0;
